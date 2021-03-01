2084742d1eb45e147f70433945f7eba53dde6fba
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-03-01 21:32:45 +0200

    step1, step2, step 3 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: ''
+}
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            Name
+            <input type='text' />
+          </label>
+          <label>
+            Number
+            <input type='text' />
+          </label>
+          <button type='submit'>Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <label>
+          Find contacts by name
+          <input type='text' /></label>
+        <ul>
+          {contacts.map(({ id, name, number }) => { return (<li key={id}>{name}: { number}</li>) })}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
